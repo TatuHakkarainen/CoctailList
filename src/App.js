19cd82203bbeb85526eb0c72e6eb5ddcfab85bd8
TatuHakkarainen <tatu.hakkarainen@gmail.com>
2023-10-04 12:05:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [cocktails, setCocktails] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchCocktail = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+      );
+      const foundCocktails = response.data.drinks || [];
+      setCocktails(foundCocktails);
+    } catch (error) {
+      console.error("Virhe hakemisessa:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
+        <h1>COCKTAILS</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>WRITE COCKTAIL NAME</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={searchCocktail}>SEARCH</button>
+      </div>
+      {cocktails.length > 0 && (
+        <div className="cocktail-list">
+          {cocktails.map((cocktail, index) => (
+            <div key={index} className="cocktail-info">
+              <h2>{cocktail.strDrink}</h2>
+              {cocktail.strDrinkThumb && (
+                <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+              )}
+              <p>
+                <strong>Category:</strong> {cocktail.strCategory}
+              </p>
+              <p>
+                <strong>Glass:</strong> {cocktail.strGlass}
+              </p>
+              <p>
+                <strong>Instructions:</strong> {cocktail.strInstructions}
+              </p>
+              <p>
+                <strong>Ingredients:</strong>
+              </p>
+              <ul>
+                {Array.from({ length: 15 }, (_, i) => {
+                  const ingredient = cocktail[`strIngredient${i + 1}`];
+                  const measure = cocktail[`strMeasure${i + 1}`];
+
+                  if (ingredient && measure) {
+                    return (
+                      <li key={i}>
+                        {ingredient} {measure}
+                      </li>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
